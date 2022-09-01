55a689c7f315dd8c98c865aff5c1fa0738a64e33
wallworkc19 <86269805+wallworkc19@users.noreply.github.com>
2022-09-01 18:48:33 -0400

    add vuetify

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
+new Vue({
+  vuetify,
+  options
+})
 
 createApp(App).use(router).mount('#app')
