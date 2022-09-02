4f531a1656c7ebed9ef0429e2f792373e2c031a1
wallworkc19 <86269805+wallworkc19@users.noreply.github.com>
2022-09-01 21:00:44 -0400

    added links and formatting

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,29 @@
 <template>
   <v-app>
-    <v-main>
+    <v-main style="background-color:#7851a9;">
       <v-toolbar color="#cfb53b">
-        <v-toolbar-title>East Carolina Sports Book</v-toolbar-title>
+        <v-toolbar-title >East Carolina Sports Book</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items>
-          <v-btn flat>Games</v-btn>
+          <v-btn flat router-link to="/">Home</v-btn>
+          <v-btn flat router-link to="/games">Games</v-btn>
           <v-btn flat>Sign Up</v-btn>
           <v-btn flat>Login</v-btn>
         </v-toolbar-items>
       </v-toolbar>
-      <v-content>
-        <HelloWorld/>
-      </v-content>
+      
       <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+
 
 export default {
     name: "App",
     data: () => ({
     //
-    }),
-    components: { HelloWorld }
+    })
 }
 </script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,164 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <table>
+            <br><br>
+            <tr>
+              <th>Game #</th>
+              <th>Date</th>
+              <th>Home/Away</th>
+              <th>Playing</th>
+              <th>Odds</th>
+              <th>Bet</th>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>9/03/22</td>
+              <td>Home</td>
+              <td>NC State</td>
+              <td>+2200</td>
+              <td><input type="number" min="0" id="myNumber" value="0"><button>Bet Points</button></td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>9/10/22</td>
+                <td>Home</td>
+                <td>Old Dominion</td>
+                <td>50</td>
+                <td><input type="number" min="0" id="myNumber" value="0"><button>Bet Points</button></td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>9/17/22</td>
+                <td>Home</td>
+                <td>Campbell</td>
+                <td>50</td>
+                <td><input type="number" min="0" id="myNumber" value="0"><button>Bet Points</button></td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>9/24/22</td>
+                <td>Home</td>
+                <td>Navy</td>
+                <td>50</td>
+                <td><input type="number" min="0" id="myNumber" value="0"><button>Bet Points</button></td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>10/01/22</td>
+                <td>Away</td>
+                <td>South Florida</td>
+                <td>50</td>
+                <td><input type="number" min="0" id="myNumber" value="0"><button>Bet Points</button></td>
+            </tr>
+            <tr>
+                <td>6</td>
+                <td>10/08/22</td>
+                <td>Away</td>
+                <td>Tulane</td>
+                <td>50</td>
+                <td><input type="number" min="0" id="myNumber" value="0"><button>Bet Points</button></td>
+            </tr>
+            <tr>
+                <td>7</td>
+                <td>10/15/22</td>
+                <td>Home</td>
+                <td>Memphis</td>
+                <td>50</td>
+                <td><input type="number" min="0" id="myNumber" value="0"><button>Bet Points</button></td>
+            </tr>
+            <tr>
+                <td>8</td>
+                <td>10/22/22</td>
+                <td>Home</td>
+                <td>UCF</td>
+                <td>50</td>
+                <td><input type="number" min="0" id="myNumber" value="0"><button>Bet Points</button></td>
+            </tr>
+            <tr>
+                <td>9</td>
+                <td>10/28/22</td>
+                <td>Away</td>
+                <td>BYU</td>
+                <td>50</td>
+                <td><input type="number" min="0" id="myNumber" value="0"><button>Bet Points</button></td>
+            </tr>
+            <tr>
+                <td>10</td>
+                <td>11/11/22</td>
+                <td>Away</td>
+                <td>Cincinnati</td>
+                <td>50</td>
+                <td><input type="number" min="0" id="myNumber" value="0"><button>Bet Points</button></td>
+            </tr>
+            <tr>
+                <td>11</td>
+                <td>11/19/22</td>
+                <td>Home</td>
+                <td>Houston</td>
+                <td>50</td>
+                <td><input type="number" min="0" id="myNumber" value="0"><button>Bet Points</button></td>
+            </tr>
+            <tr>
+                <td>12</td>
+                <td>11/26/22</td>
+                <td>Away</td>
+                <td>Temple</td>
+                <td>50</td>
+                <td><input type="number" min="0" id="myNumber" value="0"><button>Bet Points</button></td>
+            </tr>
+            <tr>
+                <td>13</td>
+                <td>12/03/22</td>
+                <td>Away</td>
+                <td>TBD</td>
+                <td>50</td>
+                <td><input type="number" min="0" id="myNumber" value="0"><button>Bet Points</button></td>
+            </tr>
+        </table>
   </div>
 </template>
+<style>
+  ul {
+      list-style-type: none;
+      font-size: large;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+      background-color: #cfb53b;
+  }
+
+  li {
+      float: left;
+  }
+
+  li a {
+      display: block;
+      color: black;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+  }
+
+  li a:hover {
+      background-color: #5C5C5C;
+  }
+  table {
+      font-family: arial, sans-serif;
+      border-collapse: collapse;
+      width: 95%;
+      margin: auto;
+  }
+
+  td, th {
+      border: 1px solid #cfb53b;
+      text-align: center;
+      padding: 15px;
+  }
+
+  th {
+      background-color: #cfb53b;
+  }
+  tr:nth-child(odd) {
+      background-color: #5C5C5C;
+  }
+</style>
